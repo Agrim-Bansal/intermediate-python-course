298d158eb62be35747ae3febebb41559944ad3bb
Agrim Bansal <agrimx2@gmail.com>
2021-01-28 10:18:21 +0530

    Added conditional comments

--- dice_roller.py
+++ dice_roller.py
@@ -4,8 +4,16 @@ def main(dice_rolls):
   dice_sum = 0
   for i in range(0, dice_rolls):
     roll = random.randint(1,6)
+    
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
+
     dice_sum += roll
-    print(f'You rolled a {roll}')
+
 
   print('You rolled a total of %s'%dice_sum)
 
